8c615661906d7a6943ecf99fe8c05654ba22d8bc
Marcelo C Alves <marcelocalves@live.com>
2021-11-11 21:55:46 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const randomNumber = max => {
+  return Math.floor(Math.random() * max);
+};
 
+const randomExcuse = (who, action, what, when) => {
+  let randomWho = randomNumber(who.length);
+  let randomAction = randomNumber(action.length);
+  let randomWhat = randomNumber(what.length);
+  let randomWhen = randomNumber(when.length);
+  let excuse = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+  return excuse;
+};
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let mainPage = document.querySelector("#main");
+  let newParagraph = document.createElement("p");
+  mainPage.appendChild(newParagraph);
+  newParagraph.innerText = randomExcuse(who, action, what, when);
 };
